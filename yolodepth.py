714a06185159f26318d238a1be181c76e37fc825
minhajfalaki <minhajfalaki@github.com>
2018-11-16 19:21:31 +0530

    yolo depth

---
+++ yolodepth.py
@@ -0,0 +1,387 @@
+from keras.models import Sequential, Model
+from keras.layers import Reshape, Activation, Conv2D, Input, MaxPooling2D, BatchNormalization, Flatten, Dense, Lambda
+from keras.layers.advanced_activations import LeakyReLU
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from keras.optimizers import SGD, Adam, RMSprop
+from keras.layers.merge import concatenate
+import matplotlib.pyplot as plt
+import keras.backend as K
+import tensorflow as tf
+import imgaug as ia
+from tqdm import tqdm
+from imgaug import augmenters as iaa
+import numpy as np
+import pickle
+import os, cv2
+from preprocessing import parse_annotation_txt, BatchGenerator
+from utils import WeightReader, decode_netout, draw_boxes
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+LABELS = ['person_in_depth_image']
+
+IMAGE_H, IMAGE_W = 480, 480
+GRID_H,  GRID_W  = 15 , 15
+BOX              = 5
+CLASS            = len(LABELS)
+CLASS_WEIGHTS    = np.ones(CLASS, dtype='float32')
+OBJ_THRESHOLD    = 0.3#0.5
+NMS_THRESHOLD    = 0.3#0.45
+ANCHORS          = [7.34264,4.87195, 9.78978,6.50238, 14.05418,9.34468, 19.59295,13.03933, 26.13363,17.39940]
+
+NO_OBJECT_SCALE  = 1.0
+OBJECT_SCALE     = 5.0
+COORD_SCALE      = 1.0
+CLASS_SCALE      = 1.0
+
+BATCH_SIZE       = 32
+WARM_UP_BATCHES  = 0
+TRUE_BOX_BUFFER  = 50
+
+
+
+# wt_path = 'Tiny_yolo_depth2.h5'  
+# the function to implement the orgnization layer (thanks to github.com/allanzelener/YAD2K)
+def space_to_depth_x2(x):
+    return tf.space_to_depth(x, block_size=2)
+
+input_image = Input(shape=(IMAGE_H, IMAGE_W, 1))
+true_boxes  = Input(shape=(1, 1, 1, TRUE_BOX_BUFFER , 4))
+
+x = Conv2D(16, (3,3), strides=(1,1), padding='same', name='conv_1', use_bias=False)(input_image)
+x = BatchNormalization(name='norm_1')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 2 - 5
+for i in range(0,4):
+    x = Conv2D(32*(2**i), (3,3), strides=(1,1), padding='same', name='conv_' + str(i+2), use_bias=False)(x)
+    x = BatchNormalization(name='norm_' + str(i+2))(x)
+    x = LeakyReLU(alpha=0.1)(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 6
+x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_6', use_bias=False)(x)
+x = BatchNormalization(name='norm_6')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = MaxPooling2D(pool_size=(2, 2), strides=(1,1), padding='same')(x)
+
+# Layer 7-8
+for i in range(0,2):
+    x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_' + str(i+7), use_bias=False)(x)
+    x = BatchNormalization(name='norm_' + str(i+7))(x)
+    x = LeakyReLU(alpha=0.1)(x)
+
+
+# Layer 9
+x = Conv2D(BOX * (4 + 1 + CLASS), (1,1), strides=(1,1), padding='same', name='conv_23')(x)
+print x.shape
+output = Reshape((GRID_H, GRID_W, BOX, 4 + 1 + CLASS))(x)
+
+# small hack to allow true_boxes to be registered when Keras build the model 
+# for more information: https://github.com/fchollet/keras/issues/2790
+output = Lambda(lambda args: args[0])([output, true_boxes])
+
+model = Model([input_image, true_boxes], output)
+
+model.summary()
+
+# weight_reader = WeightReader(wt_path)
+
+# weight_reader.reset()
+# nb_conv = 9
+
+# for i in range(1, nb_conv+1):
+#     conv_layer = model.get_layer('conv_' + str(i))
+    
+#     if i < nb_conv:
+#         norm_layer = model.get_layer('norm_' + str(i))
+        
+#         size = np.prod(norm_layer.get_weights()[0].shape)
+
+#         beta  = weight_reader.read_bytes(size)
+#         gamma = weight_reader.read_bytes(size)
+#         mean  = weight_reader.read_bytes(size)
+#         var   = weight_reader.read_bytes(size)
+
+#         weights = norm_layer.set_weights([gamma, beta, mean, var])       
+        
+#     if len(conv_layer.get_weights()) > 1:
+#         bias   = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
+#         kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+#         kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+#         kernel = kernel.transpose([2,3,1,0])
+#         conv_layer.set_weights([kernel, bias])
+#     else:
+#         kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+#         kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+#         kernel = kernel.transpose([2,3,1,0])
+#         conv_layer.set_weights([kernel])
+
+def custom_loss(y_true, y_pred):
+
+    mask_shape = tf.shape(y_true)[:4]
+    
+    cell_x = tf.to_float(tf.reshape(tf.tile(tf.range(GRID_W), [GRID_H]), (1, GRID_H, GRID_W, 1, 1)))
+    cell_y = tf.transpose(cell_x, (0,2,1,3,4))
+
+    cell_grid = tf.tile(tf.concat([cell_x,cell_y], -1), [BATCH_SIZE, 1, 1, 5, 1])
+    
+    coord_mask = tf.zeros(mask_shape)
+    conf_mask  = tf.zeros(mask_shape)
+    class_mask = tf.zeros(mask_shape)
+    
+    seen = tf.Variable(0.)
+    total_recall = tf.Variable(0.)
+    
+    """
+    Adjust prediction
+    """
+    ### adjust x and y      
+    pred_box_xy = tf.sigmoid(y_pred[..., :2]) + cell_grid
+    
+    ### adjust w and h
+    pred_box_wh = tf.exp(y_pred[..., 2:4]) * np.reshape(ANCHORS, [1,1,1,BOX,2])
+    
+    ### adjust confidence
+    pred_box_conf = tf.sigmoid(y_pred[..., 4])
+    
+    ### adjust class probabilities
+    pred_box_class = y_pred[..., 5:]
+    
+    """
+    Adjust ground truth
+    """
+    ### adjust x and y
+    true_box_xy = y_true[..., 0:2] # relative position to the containing cell
+    
+    ### adjust w and h
+    true_box_wh = y_true[..., 2:4] # number of cells accross, horizontally and vertically
+    
+    ### adjust confidence
+    true_wh_half = true_box_wh / 2.
+    true_mins    = true_box_xy - true_wh_half
+    true_maxes   = true_box_xy + true_wh_half
+    
+    pred_wh_half = pred_box_wh / 2.
+    pred_mins    = pred_box_xy - pred_wh_half
+    pred_maxes   = pred_box_xy + pred_wh_half       
+    
+    intersect_mins  = tf.maximum(pred_mins,  true_mins)
+    intersect_maxes = tf.minimum(pred_maxes, true_maxes)
+    intersect_wh    = tf.maximum(intersect_maxes - intersect_mins, 0.)
+    intersect_areas = intersect_wh[..., 0] * intersect_wh[..., 1]
+    
+    true_areas = true_box_wh[..., 0] * true_box_wh[..., 1]
+    pred_areas = pred_box_wh[..., 0] * pred_box_wh[..., 1]
+
+    union_areas = pred_areas + true_areas - intersect_areas
+    iou_scores  = tf.truediv(intersect_areas, union_areas)
+    
+    true_box_conf = iou_scores * y_true[..., 4]
+    
+    ### adjust class probabilities
+    true_box_class = tf.argmax(y_true[..., 5:], -1)
+    
+    """
+    Determine the masks
+    """
+    ### coordinate mask: simply the position of the ground truth boxes (the predictors)
+    coord_mask = tf.expand_dims(y_true[..., 4], axis=-1) * COORD_SCALE
+    
+    ### confidence mask: penelize predictors + penalize boxes with low IOU
+    # penalize the confidence of the boxes, which have IOU with some ground truth box < 0.6
+    true_xy = true_boxes[..., 0:2]
+    true_wh = true_boxes[..., 2:4]
+    
+    true_wh_half = true_wh / 2.
+    true_mins    = true_xy - true_wh_half
+    true_maxes   = true_xy + true_wh_half
+    
+    pred_xy = tf.expand_dims(pred_box_xy, 4)
+    pred_wh = tf.expand_dims(pred_box_wh, 4)
+    
+    pred_wh_half = pred_wh / 2.
+    pred_mins    = pred_xy - pred_wh_half
+    pred_maxes   = pred_xy + pred_wh_half    
+    
+    intersect_mins  = tf.maximum(pred_mins,  true_mins)
+    intersect_maxes = tf.minimum(pred_maxes, true_maxes)
+    intersect_wh    = tf.maximum(intersect_maxes - intersect_mins, 0.)
+    intersect_areas = intersect_wh[..., 0] * intersect_wh[..., 1]
+    
+    true_areas = true_wh[..., 0] * true_wh[..., 1]
+    pred_areas = pred_wh[..., 0] * pred_wh[..., 1]
+
+    union_areas = pred_areas + true_areas - intersect_areas
+    iou_scores  = tf.truediv(intersect_areas, union_areas)
+
+    best_ious = tf.reduce_max(iou_scores, axis=4)
+    conf_mask = conf_mask + tf.to_float(best_ious < 0.6) * (1 - y_true[..., 4]) * NO_OBJECT_SCALE
+    
+    # penalize the confidence of the boxes, which are reponsible for corresponding ground truth box
+    conf_mask = conf_mask + y_true[..., 4] * OBJECT_SCALE
+    
+    ### class mask: simply the position of the ground truth boxes (the predictors)
+    class_mask = y_true[..., 4] * tf.gather(CLASS_WEIGHTS, true_box_class) * CLASS_SCALE       
+    
+    """
+    Warm-up training
+    """
+    no_boxes_mask = tf.to_float(coord_mask < COORD_SCALE/2.)
+    seen = tf.assign_add(seen, 1.)
+    
+    true_box_xy, true_box_wh, coord_mask = tf.cond(tf.less(seen, WARM_UP_BATCHES), 
+                          lambda: [true_box_xy + (0.5 + cell_grid) * no_boxes_mask, 
+                                   true_box_wh + tf.ones_like(true_box_wh) * np.reshape(ANCHORS, [1,1,1,BOX,2]) * no_boxes_mask, 
+                                   tf.ones_like(coord_mask)],
+                          lambda: [true_box_xy, 
+                                   true_box_wh,
+                                   coord_mask])
+    
+    """
+    Finalize the loss
+    """
+    nb_coord_box = tf.reduce_sum(tf.to_float(coord_mask > 0.0))
+    nb_conf_box  = tf.reduce_sum(tf.to_float(conf_mask  > 0.0))
+    nb_class_box = tf.reduce_sum(tf.to_float(class_mask > 0.0))
+    
+    loss_xy    = tf.reduce_sum(tf.square(true_box_xy-pred_box_xy)     * coord_mask) / (nb_coord_box + 1e-4) / 2.
+    loss_wh    = tf.reduce_sum(tf.square(true_box_wh-pred_box_wh)     * coord_mask) / (nb_coord_box + 1e-4) / 2.
+    loss_conf  = tf.reduce_sum(tf.square(true_box_conf-pred_box_conf) * conf_mask)  / (nb_conf_box  + 1e-4) / 2.
+    loss_class = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=true_box_class, logits=pred_box_class)
+    loss_class = tf.reduce_sum(loss_class * class_mask) / (nb_class_box + 1e-4)
+    
+    loss = loss_xy + loss_wh + loss_conf + loss_class
+    
+    nb_true_box = tf.reduce_sum(y_true[..., 4])
+    nb_pred_box = tf.reduce_sum(tf.to_float(true_box_conf > 0.5) * tf.to_float(pred_box_conf > 0.3))
+
+    """
+    Debugging code
+    """    
+    current_recall = nb_pred_box/(nb_true_box + 1e-6)
+    total_recall = tf.assign_add(total_recall, current_recall) 
+
+    loss = tf.Print(loss, [tf.zeros((1))], message='Dummy Line \t', summarize=1000)
+    loss = tf.Print(loss, [loss_xy], message='Loss XY \t', summarize=1000)
+    loss = tf.Print(loss, [loss_wh], message='Loss WH \t', summarize=1000)
+    loss = tf.Print(loss, [loss_conf], message='Loss Conf \t', summarize=1000)
+    loss = tf.Print(loss, [loss_class], message='Loss Class \t', summarize=1000)
+    loss = tf.Print(loss, [loss], message='Total Loss \t', summarize=1000)
+    loss = tf.Print(loss, [current_recall], message='Current Recall \t', summarize=1000)
+    loss = tf.Print(loss, [total_recall/seen], message='Average Recall \t', summarize=1000)
+    # loss = tf.Print(loss, [y_true], message='y_true \t', summarize=1000)
+    # loss = tf.Print(loss, [y_pred], message='y_pred \t', summarize=1000)
+    # print y_true,"is y_true"
+    # print y_pred,"is y_pred"
+    
+    return loss
+
+
+
+
+generator_config = {
+    'IMAGE_H'         : IMAGE_H, 
+    'IMAGE_W'         : IMAGE_W,
+    'GRID_H'          : GRID_H,  
+    'GRID_W'          : GRID_W,
+    'BOX'             : BOX,
+    'LABELS'          : LABELS,
+    'CLASS'           : len(LABELS),
+    'ANCHORS'         : ANCHORS,
+    'IMAGE_C'         : 1,
+    'BATCH_SIZE'      : BATCH_SIZE,
+    'TRUE_BOX_BUFFER' : 50,
+}
+
+def normalize(image):
+    image = cv2.normalize(image,image, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    image = np.uint8(image)
+    image = cv2.Canny(image,10,100)
+    image = cv2.normalize(image,image, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    image = np.array(image).reshape(-1,480,480,1)
+    return image
+
+training = 1
+validate = 0
+
+train_imgs, seen_train_labels = parse_annotation_txt('training_labels1.txt',training)
+# print seen_train_labels
+# sess=tf.Session()
+train_batch = BatchGenerator(train_imgs, generator_config, norm=normalize, jitter=False)
+# print train_batch
+# print sess.run(train_batch)
+
+valid_imgs, seen_valid_labels = parse_annotation_txt('validation_labels1.txt',validate)
+valid_batch = BatchGenerator(valid_imgs, generator_config, norm=normalize, jitter=False)
+
+
+
+early_stop = EarlyStopping(monitor='val_loss', 
+                           min_delta=0.001, 
+                           patience=3, 
+                           mode='min', 
+                           verbose=1)
+
+checkpoint = ModelCheckpoint('Tiny_yolo_depth2.h5', 
+                             monitor='val_loss', 
+                             verbose=1, 
+                             save_best_only=True, 
+                             mode='min', 
+                             period=1)
+
+tb_counter  = len([log for log in os.listdir(os.path.expanduser('~/logs/')) if 'yolo_depth' in log]) + 1
+tensorboard = TensorBoard(log_dir=os.path.expanduser('~/logs/') + 'yolo_depth' + '_' + str(tb_counter), 
+                          histogram_freq=0, 
+                          write_graph=True, 
+                          write_images=False)
+
+
+optimizer = Adam(lr=0.5e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+model.compile(loss=custom_loss, optimizer=optimizer)
+
+
+model.fit_generator(generator        = train_batch, 
+                    steps_per_epoch  = len(train_batch), 
+                    epochs           = 100, 
+                    verbose          = 1,
+                    validation_data  = valid_batch,
+                    validation_steps = len(valid_batch),
+                    callbacks        = [early_stop, checkpoint, tensorboard], 
+                    max_queue_size   = 3)
+
+
+
+
+# model.load_weights("Tiny_yolo_depth2.h5")
+
+# image = cv2.imread('train1/seq0_0000_1.pgm',-1)
+# dummy_array = np.zeros((1,1,1,1,TRUE_BOX_BUFFER,4))
+
+# plt.figure(figsize=(10,10))
+
+# input_image = cv2.resize(image, (480, 480))
+# # input_image = input_image / 65287
+# input_image = cv2.normalize(input_image,input_image, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+# print input_image.shape,"is the fucking shape"
+# print input_image
+# input_image = np.array(input_image).reshape(480,480,1)
+# # x = np.array([i[0] for i in data]).reshape(-1,150,200,1)
+# input_image = input_image[:,:,::np.newaxis]
+# input_image = np.expand_dims(input_image, 0)
+
+# netout = model.predict([input_image, dummy_array])
+
+# boxes = decode_netout(netout[0], 
+#                       obj_threshold=OBJ_THRESHOLD,
+#                       nms_threshold=NMS_THRESHOLD,
+#                       anchors=ANCHORS, 
+#                       nb_class=CLASS)
+            
+# image = draw_boxes(image, boxes, labels=LABELS)
+# print boxes, "is the box"
+
+# plt.imshow(image); plt.show()
